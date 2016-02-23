24377ff216dfb9536494db92f3ba7d7864939a62
Steven Olson <steveoscaro@gmail.com>
2016-02-22 22:32:50 -0700

    fixed datetime formatting issues

--- app/controllers/api/v1/items_find_controller.rb
+++ app/controllers/api/v1/items_find_controller.rb
@@ -7,8 +7,8 @@ class Api::V1::ItemsFindController < Api::ApiController
     respond_with Item.find_by(description: params[:description]) if params[:description]
     respond_with Item.find_by(unit_price: params[:unit_price]) if params[:unit_price]
     respond_with Item.find_by(item_id: params[:item_id]) if params[:item_id]
-    respond_with Item.find_by(created_at: params[:created_at]) if params[:created_at]
-    respond_with Item.find_by(updated_at: params[:updated_at]) if params[:updated_at]
+    respond_with Item.find_by(created_at: DateTime.parse(params[:created_at])) if params[:created_at]
+    respond_with Item.find_by(updated_at: DateTime.parse(params[:created_at])) if params[:updated_at]
   end
 
   def index
@@ -16,8 +16,8 @@ class Api::V1::ItemsFindController < Api::ApiController
     respond_with Item.where("lower(items.description) LIKE '#{params[:description].downcase}'") if params[:description]
     respond_with Item.where("lower(items.unit_price) LIKE '#{params[:unit_price].downcase}'") if params[:unit_price]
     respond_with Item.where("items.merchant_id LIKE '#{params[:merchant_id]}'") if params[:merchant_id]
-    respond_with Item.where("lower(items.created_at) LIKE '#{params[:created_at].downcase}'") if params[:created_at]
-    respond_with Item.where("lower(items.updated_at) LIKE '#{params[:updated_at].downcase}'") if params[:updated_at]
+    respond_with Item.find_by(created_at: DateTime.parse(params[:created_at])) if params[:created_at]
+    respond_with Item.find_by(created_at: DateTime.parse(params[:updated_at])) if params[:updated_at]
     respond_with Item.find(params[:id]) if params[:id]
   end
 
